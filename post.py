ad417832c4cd0ad8230f15175d0a088970b6ff5c
Cristian Ruiz <cristianruizvalerod@gmail.com>
2024-02-03 14:31:27 -0300

    modificacion del metodo  post

--- post.py
+++ post.py
@@ -4,5 +4,15 @@ data = {'title':'Mi prueba',
         'body':'Este es el body de un blog cualquiera con su contenido',
         'userId':1
         }
-req = requests.post("https://jsonplaceholder.typicode.com/posts",data=data)
+req = requests.post("https://jsonplaceholder.typicode.com/posts", data=data)
+"""Se crea el post dentro del servidor """
 print(f"respuesta en código del servidor : {req.status_code}")
+"""Retorna el status code"""
+
+
+req = requests.get("https://jsonplaceholder.typicode.com/posts")
+"""Se listan los posts """
+print(req.content)
+"""se pide el contenido de la petición"""
+print(f"respuesta en código del servidor : {req.status_code}")
+"""se hace el requerimiento del status code"""
